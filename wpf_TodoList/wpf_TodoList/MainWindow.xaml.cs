f96c35b1200bad7a831ba7c973ee9b9569d3935d
190447062 <105758333+190447062@users.noreply.github.com>
2025-12-06 12:39:35 +0900

    [Add]Todoリストの作製

---
+++ wpf_TodoList/wpf_TodoList/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_TodoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Todoアイテムリスト
+        /// </summary>
+        private readonly List<TodoItem> todoItems = new();
+
+        /// <summary>
+        /// ID番号初期値
+        /// </summary>
+        private int idNumber = 1;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // TodoリストのItemsSourceにバインド
+            TodoListBox.ItemsSource = todoItems;
+        }
+
+        /// <summary>
+        /// Todoアイテムリスト
+        /// </summary>
+
+        /// <summary>
+        /// 追加ボタン押下時の処理
+        /// </summary>
+        /// <param name="sender">イベントを発生させたオブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void TodoAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string todoText = TodoTextBox.Text;
+
+            if(string.IsNullOrWhiteSpace(todoText))
+            {
+                this.Close();
+                return;
+            }
+
+            todoItems.Add(new TodoItem
+            {
+                Id = idNumber++,
+                TodoText = todoText,
+                IsCompleted = false
+            });
+
+            TodoListBox.Items.Refresh();
+            TodoTextBox.Clear();
+        }
+
+        /// <summary>
+        /// コンテキストメニューの削除ボタン押下時の処理
+        /// </summary>
+        /// <param name="sender">イベントを発生させたオブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            todoItems.Remove((TodoItem)TodoListBox.SelectedItem);
+            TodoListBox.Items.Refresh();
+        }
+    }
+}
---
+++ wpf_TodoList/wpf_TodoList/TodoItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wpf_TodoList
+{
+    /// <summary>
+    /// Todoアイテムクラス
+    /// </summary>
+    public class TodoItem
+    {
+        /// <summary>
+        /// Idプロパティ
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Todoテキストプロパティ
+        /// </summary>
+        public string TodoText { get; set; }
+
+        /// <summary>
+        /// IsCompletedプロパティ
+        /// </summary>
+        public bool IsCompleted { get; set; }
+    }
+}
