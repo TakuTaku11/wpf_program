ac2f80c39677f371cd4108040326ac550ccb8666
190447062 <105758333+190447062@users.noreply.github.com>
2025-12-07 12:31:55 +0900

    [Add]デジタル時計の作製

---
+++ wpf_realtime/wpf_realtime/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace wpf_realtime
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// DispatcherTimerのインスタンス
+        /// </summary>
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Start();
+        }
+
+        /// <summary>
+        /// 現在時刻の表示更新処理
+        /// </summary>
+        /// <param name="sender">イベントを発生させたオブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void DispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            this.TimeTextBlock.Text = now.ToString("HH:mm:ss");
+        }
+
+        /// <summary>
+        /// 終了ボタン押下時の処理
+        /// </summary>
+        /// <param name="sender">イベントを発生させたオブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void ShutdownButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
